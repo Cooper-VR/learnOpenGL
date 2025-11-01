9f3f79fe610e796e1998bfc510ca4f50cc5fb0ed
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2025-11-01 13:09:30 -0700

    add matrial to shader

--- main.cpp
+++ main.cpp
@@ -28,8 +28,10 @@ bool firstMouse = true;
 float deltaTime = 0.0f;	
 float lastFrame = 0.0f;
 
-glm::vec3 BaselightPos(1.2f, 1.0f, 2.0f);
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+glm::vec3 lightColor;
+
 
 int main()
 {
@@ -154,6 +156,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
+        lightColor.x = sin(glfwGetTime() * 2.0f);
+        lightColor.y = sin(glfwGetTime() * 0.7f);
+        lightColor.z = sin(glfwGetTime() * 1.3f);
+        glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); 
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); 
+
         lightingShader.use();
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
@@ -163,6 +171,9 @@ int main()
         lightingShader.setVec3("matrial.diffuse", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("matrial.specular", 0.5f, 0.5f, 0.5f);
         lightingShader.setFloat("matrial.shininess", 32);
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
--- resources/shaders/objectLighting_fragment.glsl
+++ resources/shaders/objectLighting_fragment.glsl
@@ -13,23 +13,32 @@ struct Matrial{
     float shininess;
 };
 
+struct Light{
+    vec3 position;
+
+    vec3 ambiant;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
 uniform Matrial matrial;
 
 in vec3 Normal;
 in vec3 FragPos;
 
 void main(){
-    vec3 ambiant = matrial.ambiant * lightColor;
+    vec3 ambiant = matrial.ambiant * light.ambiant;
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = lightColor * (diff * matrial.diffuse);
+    vec3 diffuse = light.diffuse * (diff * matrial.diffuse);
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), matrial,shininess);
-    vec3 specular = lightColor * (spec * matrial.specular);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), matrial.shininess);
+    vec3 specular = light.specular * (spec * matrial.specular);
 
     vec3 result = (ambiant + diffuse + specular);
     fragColor = vec4(result, 1.0);
